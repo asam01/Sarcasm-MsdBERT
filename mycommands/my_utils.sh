2b14590825168bcb85471846b682ef32e1eaf525
Neha Nishikant <nnishika@tir.lti.cs.cmu.edu>
2022-11-07 19:28:06 -0500

    added bash files

---
+++ mycommands/my_utils.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+#SBATCH --mem=16gb                   # Job memory request
+#SBATCH --time=0                      # Time limit hrs:min:sec
+#SBATCH --gres=gpu:1                # Number of gpu
+#SBATCH --cpus-per-task=4            # Number of CPU cores per task
+#SBATCH --output=./log/%j.log   # Standard output and error log, the program output will be here
+​
+# you can always have this
+eval "$(conda shell.bash hook)"
+# you environment
+source /home/nnishika/miniconda3/etc/profile.d/conda.sh
+conda activate msdbert
+​
+export TQDM_DISABLE=1
+# code
+
+python3 my_utils.py
